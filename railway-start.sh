06bc5009ec15d425a1e726d065a00c708ba9a461
nsdalinding-ship-it <nsdalinding@paterostechnologicalcollege.edu.ph>
2025-12-12 08:46:46 +0800

    Add commands to clear views, config, and cache

--- railway-start.sh
+++ railway-start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 php artisan optimize:clear
+php artisan view:clear
+php artisan config:clear
+php artisan cache:clear
 php artisan storage:link
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8080
